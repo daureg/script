65d239b9268ed5afc1d8788790a08dedd24a7f60
Géraud Le Falher <Graud Le Falher daureg@gmail.com>
2010-06-19 21:25:44 +0200

    radeonhd n'est plus développé

--- icatalyst.sh
+++ icatalyst.sh
@@ -1,6 +1,6 @@
-sudo pacman -S catalyst
+sudo pacman -S catalyst-xorg18/catalyst
 sudo sed -i -e '/catalyst/ s/^#//' /etc/rc.conf
-sudo sed -i -e '/radeonhd/ s/^/#/' /etc/rc.conf
+sudo sed -i -e '/radeon-hint/ s/^/#/' /etc/rc.conf
 sudo sed -i -e '/catalyst/ s/^#//' /etc/X11/xorg.conf
-sudo sed -i -e '/radeonhd-hint/ s/^/#/' /etc/X11/xorg.conf
+sudo sed -i -e '/radeon-hint/ s/^/#/' /etc/X11/xorg.conf
 echo "Redemarrer sur le noyau officiel"
--- iradeon.sh
+++ iradeon.sh
@@ -1,6 +1,6 @@
-sudo pacman -Rnsdf libstdc++5 xinetd netkit-bsd-finger catalyst-utils catalyst
-sudo pacman -S libgl
+sudo pacman -Rnsdf libstdc++5 xinetd netkit-bsd-finger catalyst
+sudo pacman -S libgl ati-dri xf86-video-ati testing/xorg-xserver
 sudo sed -i -e '/catalyst/ s/^/#/' /etc/rc.conf
-sudo sed -i -e '/radeonhd/ s/^#//' /etc/rc.conf
+sudo sed -i -e '/radeon-hint/ s/^#//' /etc/rc.conf
 sudo sed -i -e '/catalyst/ s/^/#/' /etc/X11/xorg.conf
-sudo sed -i -e '/radeonhd-hint/ s/^#//' /etc/X11/xorg.conf
+sudo sed -i -e '/radeon-hint/ s/^#//' /etc/X11/xorg.conf
